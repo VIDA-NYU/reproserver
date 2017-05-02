d5bb1ced97a4afdc0ef386736d54cca2af5ea7a5
Remi Rampin <remirampin@gmail.com>
2017-05-01 21:36:06 -0400

    Connect builder to AMQP

--- builder/builder/main.py
+++ builder/builder/main.py
@@ -1,5 +1,24 @@
+import logging
 import pika
 
 
+def build(channel, method, properties, body):
+    logging.info("Build request received: %r, %r", body, properties)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
 def main():
-    pass
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Connecting to AMQP broker")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='rabbitmq', credentials=pika.PlainCredentials('admin', 'hackme')))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='build_queue', durable=True)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(build, queue='build_queue')
+
+    logging.info("Ready, listening for requests")
+    channel.start_consuming()
