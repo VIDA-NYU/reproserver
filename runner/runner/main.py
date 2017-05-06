efca60a090aa611e5aa536dc2dfcdede2489acd7
Remi Rampin <remirampin@gmail.com>
2017-05-06 17:03:47 -0400

    WIP runner

--- runner/runner/main.py
+++ runner/runner/main.py
@@ -1,5 +1,25 @@
-import time
+import logging
+import pika
+
+
+def run(channel, method, properties, body):
+    logging.info("Run request received: %r, %r", body, properties)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
 def main():
-    time.sleep(100000)
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Connecting to AMQP broker")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='reproserver-rabbitmq',
+        credentials=pika.PlainCredentials('admin', 'hackme')))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='run_queue', durable=True)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(run, queue='run_queue')
+
+    logging.info("Ready, listening for requests")
+    channel.start_consuming()
