8f8f13273d46bebb1794edca24ad51ac8f19efe7
Remi Rampin <remirampin@gmail.com>
2017-05-16 12:43:31 -0400

    Fix templates, use url_for()

--- common/common/database.py
+++ common/common/database.py
@@ -68,8 +68,9 @@ class Upload(Base):
     timestamp = Column(DateTime, nullable=False,
                        server_default=functions.now())
 
+    @property
     def experiment_code(self):
-        base64.urlsafe_b64encode(self.experiment_hash + '|' + self.filename)
+        return base64.urlsafe_b64encode(self.experiment_hash + '|' + self.filename)
 
 
 class Parameter(Base):
