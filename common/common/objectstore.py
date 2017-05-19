74a763b01e597dfaa5d5b8be52b307527ae8e6ee
Remi Rampin <remirampin@gmail.com>
2017-05-19 19:25:16 -0400

    Better deployment configuration

--- common/common/objectstore.py
+++ common/common/objectstore.py
@@ -5,6 +5,7 @@ import os
 
 def get_object_store():
     logging.info("Logging in to S3")
-    return boto3.resource('s3', endpoint_url=os.environ.get('S3_URL'),
+    s3_url = os.environ.get('S3_URL') or None
+    return boto3.resource('s3', endpoint_url=s3_url,
                           aws_access_key_id=os.environ['S3_KEY'],
                           aws_secret_access_key=os.environ['S3_SECRET'])
