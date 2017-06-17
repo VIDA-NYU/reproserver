739570bd53cbf413b79952bf665e6ba4bc6499d5
Remi Rampin <remirampin@gmail.com>
2017-06-17 16:23:03 -0400

    Fix logging WSGI app startup

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -5,11 +5,15 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 
-from common.utils import setup_logging
-from web.main import app  # noqa
+from common.utils import setup_logging  # noqa
 
 
 setup_logging('REPROSERVER-WEB')
+
+
+from web.main import app  # noqa
+
+
 application = app
 
 __all__ = ['application']
