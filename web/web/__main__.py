205a5ef7390da7ff10a9465b386e0dd1c4329694
Remi Rampin <remirampin@gmail.com>
2017-06-18 18:30:00 -0400

    Fix logging web app startup
    Similar to 739570bd53cbf413b79952bf665e6ba4bc6499d5, who did this for
    the wsgi entrypoint, set logger up before importing app from __main__.

--- web/web/__main__.py
+++ web/web/__main__.py
@@ -6,8 +6,9 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 
 from common.utils import setup_logging  # noqa
-from web.main import main  # noqa
-
 
 setup_logging('REPROSERVER-WEB')
+
+from web.main import main  # noqa
+
 main()
