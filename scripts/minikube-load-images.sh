339e7a86f7b4b336ffec5d8ff772d18e2604f398
Remi Rampin <r@remirampin.com>
2019-08-15 17:15:11 -0400

    Add script to load images into Minikube VM

---
+++ scripts/minikube-load-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+if [ "x${DOCKER_HOST-}" = x ]; then
+    echo "DOCKER_HOST is not set; running 'eval \$(minikube docker-env)" >&2
+    eval $(minikube docker-env)
+else
+    echo "DOCKER_HOST is set" >&2
+fi
+
+for image in reproserver_web reproserver_builder reproserver_runner; do
+    echo "Loading image $image..."
+    DOCKER_HOST= sudo -g docker docker save $image | docker load
+done
