a3bba71a1e88b199384d61f8253b6634e4cd8eb6
Remi Rampin <r@remirampin.com>
2019-09-10 18:41:33 -0400

    Implement parse_url() for Figshare

--- reproserver/repositories/figshare.py
+++ reproserver/repositories/figshare.py
@@ -9,6 +9,15 @@ from .base import BaseRepository, RepositoryError
 logger = logging.getLogger(__name__)
 
 
+# One format:
+# https://figshare.com/articles/name_name/3546675 (assumes one RPZ file)
+# There is no URL that includes both article and file
+# Direct link to files look like this (but no way to use API from that):
+# https://ndownloader.figshare.com/files/5612292
+_figshare_url = re.compile(
+    r'https?://figshare\.com/articles/[^/]+/([0-9]+)$'
+)
+
 _figshare_path = re.compile(r'^([0-9]+)/files/([0-9]+)$')
 
 
@@ -17,17 +26,40 @@ class Figshare(BaseRepository):
     URL_DOMAINS = ['figshare.com']
 
     async def parse_url(self, url):
-        if url.startswith('http://'):
-            url = url[7:]
-        elif url.startswith('https://'):
-            url = url[8:]
-        else:
-            raise RepositoryError("Invalid URL")
-        if url.lower().startswith('figshare.com/'):
-            raise RepositoryError("Not Figshare URL")
+        m = _figshare_url.match(url)
+        if m is None:
+            raise RepositoryError("Invalid Figshare URL")
+        article = m.group(1)
 
-        # TODO: Actually need to find the article ID, which is not in the URL?
-        raise NotImplementedError
+        # Get the list of files, proceed if there's only one RPZ file
+        resp = await self.http_client.fetch(
+            'https://api.figshare.com/v2/articles/{0}/files'.format(article),
+            headers={
+                'Accept': 'application/json',
+                'User-Agent': 'reproserver %s' % __version__,
+            },
+            raise_error=False,
+        )
+        if resp.code != 200:
+            logger.info("Got error %s", resp.code)
+            raise RepositoryError("HTTP error from Figshare")
+        try:
+            files = json.loads(resp.body.decode('utf-8'))
+        except ValueError:
+            logger.error("Got invalid JSON from figshare.com")
+            raise RepositoryError("Invalid JSON returned from Figshare")
+
+        logger.info("Fetched file list, %d files", len(files))
+        files = [f for f in files
+                 if f['name'].lower().endswith('.rpz')]
+        if not files:
+            raise RepositoryError("No RPZ file in that item")
+        elif len(files) > 1:
+            raise RepositoryError("Multiple RPZ files in that item")
+        else:
+            file_id = files[0]['id']
+            logger.info("Single RPZ selected: %r", files[0]['name'])
+            return 'figshare.com', '{0}/files/{1}'.format(article, file_id)
 
     async def get_experiment(self, db, object_store, remote_addr,
                              repo, repo_path):
