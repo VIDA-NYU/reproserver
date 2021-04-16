3cc1a547d40b38cabf5db30d186514e5f7ba8ecf
Remi Rampin <remi@rampin.org>
2021-04-16 15:01:46 -0400

    Complete integration test

--- tests/integ/test_integ.py
+++ tests/integ/test_integ.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 import tempfile
+import time
 import unittest
 
 
@@ -61,3 +62,33 @@ class TestRepro(ReproserverTest):
 
             # Post the file
             res = self.reproserver_post('/upload', files={'rpz_file': tmp})
+            content = res.content.decode('utf-8')
+            self.assertIn('Command-line for step run0', content)
+
+            # Have it run
+            m = re.search(r'/reproduce/([a-z0-9]{5})$', res.url)
+            upload_short_id = m.group(1)
+            res = self.reproserver_post(
+                '/run/{0}'.format(upload_short_id),
+                data={'param_cmdline_00000': './count.sh'},
+            )
+
+            # Get results
+            m = re.search(r'/results/([a-z0-9]{5})$', res.url)
+            run_short_id = m.group(1)
+            obj = None
+            for i in range(20):
+                time.sleep(2)
+                res = self.reproserver_get(
+                    '/results/{0}?log_from=0'.format(run_short_id),
+                    headers={'Accept': 'application/json'},
+                )
+                obj = res.json()
+                if obj['done']:
+                    break
+            else:
+                self.fail("Run didn't complete: %r" % (obj,))
+            self.assertEqual(
+                obj['log'],
+                ['5', '*** Command finished, status: 0'],
+            )
