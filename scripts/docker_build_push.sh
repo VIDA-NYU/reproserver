1ea5b8bb8085a1c03d39c64d4c4e504d9c3d7296
Remi Rampin <remi@rampin.org>
2021-06-24 17:24:18 -0400

    Add Docker build script

---
+++ scripts/docker_build_push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+cd "$(dirname "$0")/.."
+
+VERSION=$(git describe)
+
+IMAGE=ghcr.io/vida-nyu/reproserver/web:$VERSION
+
+docker build -t $IMAGE .
+docker push $IMAGE
+
+echo
+echo "    $IMAGE"
